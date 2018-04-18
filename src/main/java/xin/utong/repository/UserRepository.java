31bde1b17b6223c580500a2ec7fb0282a17f18b8
yutong <475270763@qq.com>
2018-04-18 11:42:36 +0800

    Repository层调整

--- src/main/java/xin/utong/repository/UserRepository.java
+++ src/main/java/xin/utong/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package xin.utong.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import xin.utong.model.UserVo;
 
 /**
  * user dao
  * Created by yutong on 2017/7/11.
  */
-@Repository
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends MongoRepository<UserVo, String> {
     UserVo findUserVoByUsername(final String username);
 }
